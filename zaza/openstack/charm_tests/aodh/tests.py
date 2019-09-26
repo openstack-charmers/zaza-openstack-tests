3ded7dab36351672ec0a9cc10a42b8b737f8a599
Liam Young <liam.young@canonical.com>
2019-09-26 09:27:11 +0000

    Fix copy/pasta error

--- zaza/openstack/charm_tests/aodh/tests.py
+++ zaza/openstack/charm_tests/aodh/tests.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Encapsulate masakari testing."""
+"""Encapsulate Aodh testing."""
 
 import logging
 
