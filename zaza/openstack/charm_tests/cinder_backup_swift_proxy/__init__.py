d004a4152d048ae72c51393da7aabcb39c71c339
Andrew McLeod <andrew.mcleod@canonical.com>
2019-11-28 16:45:44 +0100

    rename cinder backup swift to cinder backup swift proxy (#134)

--- zaza/openstack/charm_tests/cinder_backup_swift_proxy/__init__.py
+++ zaza/openstack/charm_tests/cinder_backup_swift_proxy/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Collection of code for setting up and testing cinder-backup-swift."""
+"""Collection of code for setting up and testing cinder-backup-swift-proxy."""
