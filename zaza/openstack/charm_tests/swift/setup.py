4b9773bb5ecd94120083df1c86fd37e0d31d03d3
Liam Young <liam.young@canonical.com>
2020-01-27 10:10:43 +0000

    Fix copy/pasta error

--- zaza/openstack/charm_tests/swift/setup.py
+++ zaza/openstack/charm_tests/swift/setup.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Code for configuring octavia."""
+"""Code for configuring swift."""
 
 import logging
 import tenacity
