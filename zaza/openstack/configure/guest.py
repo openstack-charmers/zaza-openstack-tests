83246a93050b3d6f1bd591aaeae11c4ab67fc256
Aurelien Lourot <aurelien.lourot@canonical.com>
2020-04-09 17:02:49 +0200

    Print stderr when failing to ping new instance

--- zaza/openstack/configure/guest.py
+++ zaza/openstack/configure/guest.py
@@ -16,6 +16,7 @@
 
 """Encapsulate nova testing."""
 
+import subprocess
 import logging
 import time
 
@@ -132,7 +133,13 @@ def launch_instance(instance_key, use_boot_volume=False, vm_name=None,
         external_network_name,
         port=port)['floating_ip_address']
     logging.info('Assigned floating IP {} to {}'.format(ip, vm_name))
-    openstack_utils.ping_response(ip)
+    try:
+        openstack_utils.ping_response(ip)
+    except subprocess.CalledProcessError as e:
+        logging.error('Pinging {} failed with {}'.format(ip, e.returncode))
+        logging.error('stdout: {}'.format(e.stdout))
+        logging.error('stderr: {}'.format(e.stderr))
+        raise
 
     # Check ssh'ing to instance.
     logging.info('Testing ssh access.')
