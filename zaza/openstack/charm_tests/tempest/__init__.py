db47aa1fb9fa627481bcd14e8cc947a6eadfe267
Corey Bryant <corey.bryant@canonical.com>
2020-05-18 20:10:52 +0000

    Minor update

--- zaza/openstack/charm_tests/tempest/__init__.py
+++ zaza/openstack/charm_tests/tempest/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Collection of code for setting up and using tempest."""
+"""Collection of code for setting up and running tempest."""
