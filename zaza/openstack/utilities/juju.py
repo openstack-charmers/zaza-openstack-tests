5401be3a9700782d84bef0f78cdd3ccc06d6378a
Corey Bryant <corey.bryant@canonical.com>
2020-06-05 20:04:51 +0000

    Fix variable name

--- zaza/openstack/utilities/juju.py
+++ zaza/openstack/utilities/juju.py
@@ -65,7 +65,7 @@ def get_application_ip(application):
     if vip:
         ip = vip
     else:
-        unit = model.get_units(application_name)[0]
+        unit = model.get_units(application)[0]
         ip = unit.public_address
     return ip
 
