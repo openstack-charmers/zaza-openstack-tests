7b94eb6ebed3b2b4d703ec0ff98e21644d166411
Aurelien Lourot <aurelien.lourot@canonical.com>
2020-09-07 11:40:50 +0200

    Make HaclusterScalebackTest less bundle-dependent

--- zaza/openstack/charm_tests/hacluster/tests.py
+++ zaza/openstack/charm_tests/hacluster/tests.py
@@ -79,93 +79,54 @@ class HaclusterTest(HaclusterBaseTest):
 class HaclusterScalebackTest(HaclusterBaseTest):
     """hacluster scaleback tests."""
 
-    _PRINCIPLE_APP_NAME = 'keystone'
-    _HACLUSTER_APP_NAME = 'hacluster'
-    _HACLUSTER_CHARM_NAME = 'hacluster'
+    @classmethod
+    def setUpClass(cls):
+        """Run class setup for running hacluster scaleback tests."""
+        super(HaclusterScalebackTest, cls).setUpClass()
+        test_config = cls.test_config['tests_options']['hacluster']
+        cls._principle_app_name = test_config['principle-app-name']
+        cls._hacluster_charm_name = test_config['hacluster-charm-name']
 
     def test_930_scaleback(self):
-        """Remove a unit, recalculate quorum and add a new one."""
-        principle_units = zaza.model.get_status().applications[
-            self._PRINCIPLE_APP_NAME]['units']
+        """Remove a unit and add a new one."""
+        principle_units = sorted(zaza.model.get_status().applications[
+            self._principle_app_name]['units'].keys())
         self.assertEqual(len(principle_units), 3)
-        doomed_principle = sorted(principle_units.keys())[0]
-        doomed_unit = juju_utils.get_subordinate_units(
-            [doomed_principle], charm_name=self._HACLUSTER_CHARM_NAME)[0]
+        doomed_principle_unit = principle_units[0]
+        other_principle_unit = principle_units[1]
+        doomed_hacluster_unit = juju_utils.get_subordinate_units(
+            [doomed_principle_unit], charm_name=self._hacluster_charm_name)[0]
+        other_hacluster_unit = juju_utils.get_subordinate_units(
+            [other_principle_unit], charm_name=self._hacluster_charm_name)[0]
 
-        logging.info('Pausing unit {}'.format(doomed_unit))
+        logging.info('Pausing unit {}'.format(doomed_hacluster_unit))
         zaza.model.run_action(
-            doomed_unit,
+            doomed_hacluster_unit,
             'pause',
             raise_on_failure=True)
         logging.info('OK')
 
-        logging.info('Removing {}'.format(doomed_principle))
+        logging.info('Removing {}'.format(doomed_principle_unit))
         zaza.model.destroy_unit(
-            self._PRINCIPLE_APP_NAME,
-            doomed_principle,
+            self._principle_app_name,
+            doomed_principle_unit,
             wait_disappear=True)
         logging.info('OK')
 
-        expected_states = {
-            self._HACLUSTER_APP_NAME: {
-                "workload-status": "blocked",
-                "workload-status-message":
-                    "Insufficient peer units for ha cluster (require 3)"
-            },
-            self._PRINCIPLE_APP_NAME: {
-                "workload-status": "blocked",
-                "workload-status-message": "Database not initialised",
-            },
-
-            # NOTE(lourot): these applications are present in the zaza bundles
-            # used to test mysql-router, against which we also run this test:
-            'glance': {
-                "workload-status": "waiting",
-                "workload-status-message": "Incomplete relations: identity",
-            },
-            'neutron-api': {
-                "workload-status": "waiting",
-                "workload-status-message": "Incomplete relations: identity",
-            },
-            'nova-cloud-controller': {
-                "workload-status": "waiting",
-                "workload-status-message": "Incomplete relations: identity",
-            },
-        }
-        zaza.model.wait_for_application_states(states=expected_states)
+        logging.info('Waiting for model to settle')
+        zaza.model.block_until_unit_wl_status(other_hacluster_unit, 'blocked')
+        zaza.model.block_until_unit_wl_status(other_principle_unit, 'blocked')
         zaza.model.block_until_all_units_idle()
         logging.info('OK')
 
-        logging.info('Adding a hacluster unit')
-        zaza.model.add_unit(self._PRINCIPLE_APP_NAME, wait_appear=True)
-        expected_states = {
-            self._HACLUSTER_APP_NAME: {
-                "workload-status": "active",
-                "workload-status-message": "Unit is ready and clustered"
-            },
+        logging.info('Adding an hacluster unit')
+        zaza.model.add_unit(self._principle_app_name, wait_appear=True)
+        logging.info('OK')
 
-            # NOTE(lourot): these applications remain waiting/blocked after
-            # scaling back up until lp:1400481 is solved.
-            'glance': {
-                "workload-status": "waiting",
-                "workload-status-message": "Incomplete relations: identity",
-            },
-            'keystone': {
-                "workload-status": "blocked",
-                "workload-status-message": "Database not initialised",
-            },
-            'neutron-api': {
-                "workload-status": "waiting",
-                "workload-status-message": "Incomplete relations: identity",
-            },
-            'nova-cloud-controller': {
-                "workload-status": "waiting",
-                "workload-status-message": "Incomplete relations: identity",
-            },
-            'placement': {
-                "workload-status": "waiting",
-                "workload-status-message": "'identity-service' incomplete",
-            },
-        }
-        zaza.model.wait_for_application_states(states=expected_states)
+        logging.info('Waiting for model to settle')
+        zaza.model.block_until_unit_wl_status(other_hacluster_unit, 'active')
+        # NOTE(lourot): the principle application remains blocked after scaling
+        # back up until lp:1400481 is solved.
+        zaza.model.block_until_unit_wl_status(other_principle_unit, 'blocked')
+        zaza.model.block_until_all_units_idle()
         logging.debug('OK')
