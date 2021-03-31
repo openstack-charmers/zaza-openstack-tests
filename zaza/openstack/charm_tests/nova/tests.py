a54818ed7d7e032a916b7ddee4b64b8b3befe393
Martin Kalcok <martin.kalcok@canonical.com>
2021-03-31 13:25:48 +0200

    Functional tests for `instance-count` action on nova-compute charm.

--- zaza/openstack/charm_tests/nova/tests.py
+++ zaza/openstack/charm_tests/nova/tests.py
@@ -209,6 +209,38 @@ class CloudActions(test_utils.OpenStackBaseTest):
             self.fail("nova-compute service was not re-registered to the "
                       "nova-cloud-controller as expected.")
 
+    def test_instance_count_action(self):
+        """Test that action 'instance-count' returns expected values."""
+        def check_instance_count(expect_count, unit_name):
+            """Assert that unit with 'unit_name' has 'expect_count' of VMs.
+
+            :param expect_count: How many VMs are expected to be running
+            :param unit_name: Name of the target nova-compute unit
+            :return: None
+            :raises AssertionError: If result of the 'instance-count' action
+                                    does not match 'expect_count'.
+            """
+            logging.debug('Running "instance-count" action on unit "{}".'
+                          'Expecting result: {}'.format(unit_name,
+                                                        expect_count))
+            result = zaza.model.run_action(unit_name, 'instance-count')
+            self.assertEqual(result.status, 'completed')
+            instances = result.data.get('results', {}).get('instance-count')
+            self.assertEqual(instances, str(expect_count))
+
+        nova_unit = zaza.model.get_units('nova-compute',
+                                         model_name=self.model_name)[0]
+
+        check_instance_count(0, nova_unit.entity_id)
+
+        self.RESOURCE_PREFIX = 'zaza-nova'
+        self.launch_guest(
+            'ubuntu', instance_key=glance_setup.LTS_IMAGE_NAME)
+
+        check_instance_count(1, nova_unit.entity_id)
+
+        self.resource_cleanup()
+
 
 class NovaCompute(test_utils.OpenStackBaseTest):
     """Run nova-compute specific tests."""
