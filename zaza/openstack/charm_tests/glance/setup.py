be1706cb76d6cc2f7d7b20bbf3d69e63f21e8cd1
Nobuto Murata <nobuto.murata@canonical.com>
2021-05-21 15:19:28 +0900

    Functional testing for glance external S3 backend
    Set up Ceph-Radosgw in the same model as an external S3 backend for
    Glance:
    - create an S3 user in Radosgw
    - create an S3 bucket for Glance
    - inject those parameters into Glance charm configurations as an
      external S3 backend
    - upload an image to the S3 backend via Glance, and check the size of
      the image in the S3 bucket directly
    
    Co-authored-by: Connor Chamberlain <connor.chamberlain@canonical.com>

--- zaza/openstack/charm_tests/glance/setup.py
+++ zaza/openstack/charm_tests/glance/setup.py
@@ -14,7 +14,11 @@
 
 """Code for configuring glance."""
 
+import json
 import logging
+
+import boto3
+import zaza.model as model
 import zaza.openstack.utilities.openstack as openstack_utils
 import zaza.utilities.deployment_env as deployment_env
 
@@ -161,3 +165,58 @@ def add_lts_image(glance_client=None, image_name=None, release=None,
               glance_client=glance_client,
               image_name=image_name,
               properties=properties)
+
+
+def configure_external_s3_backend():
+    """Set up Ceph-radosgw as an external S3 backend for Glance."""
+    logging.info("Creating a test S3 user and credentials for Glance")
+    username, displayname = "zaza-glance-test", "Zaza Glance Test User"
+    cmd = "radosgw-admin user create --uid='{}' --display-name='{}'".format(
+        username, displayname
+    )
+    results = model.run_on_leader("ceph-mon", cmd)
+    stdout = json.loads(results["stdout"])
+    keys = stdout["keys"][0]
+    access_key, secret_key = keys["access_key"], keys["secret_key"]
+
+    logging.info("Getting S3 endpoint URL of Radosgw from Keystone")
+    keystone_auth = openstack_utils.get_overcloud_auth()
+    keystone_client = openstack_utils.get_keystone_client(keystone_auth)
+    endpoint_url = keystone_client.session.get_endpoint(
+        service_type="s3",
+        interface="public",
+        region="RegionOne",
+    )
+
+    logging.info("Creating a test S3 bucketfor Glance")
+    bucket_name = "zaza-glance-s3-test"
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url=endpoint_url,
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+    )
+    s3_client.create_bucket(Bucket=bucket_name)
+
+    logging.info("Updating Glance configs with S3 endpoint infomation")
+    model.set_application_config(
+        "glance",
+        {
+            "s3-store-host": endpoint_url,
+            "s3-store-access-key": access_key,
+            "s3-store-secret-key": secret_key,
+            "s3-store-bucket": bucket_name,
+        },
+    )
+    model.wait_for_agent_status()
+
+    logging.info("Waiting for units to reach target states")
+    model.wait_for_application_states(
+        states={
+            "glance": {
+                "workload-status": "active",
+                "workload-status-message": "Unit is ready",
+            }
+        }
+    )
+    model.block_until_all_units_idle()
