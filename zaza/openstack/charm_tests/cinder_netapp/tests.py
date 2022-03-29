df91c1e6cb76bad58a9900c1a7033ba059123ddb
Liam Young <liam.young@canonical.com>
2022-03-29 16:17:16 +0000

    Fix netapp tests
    * Check the charm config options when constructing the expected
      cinder.conf contents. This makes the tests less brittle when
      bundles are changed.
    
    * Only check the volume backend name for netapp as the host part
      is now the cinder units hostname

--- zaza/openstack/charm_tests/cinder_netapp/tests.py
+++ zaza/openstack/charm_tests/cinder_netapp/tests.py
@@ -16,19 +16,33 @@
 
 """Encapsulate cinder-netapp testing."""
 
+import zaza.model
 from zaza.openstack.charm_tests.cinder_backend.tests import CinderBackendTest
 
 
 class CinderNetAppTest(CinderBackendTest):
     """Encapsulate netapp tests."""
 
-    backend_name = 'cinder-netapp'
-
-    expected_config_content = {
+    configs = zaza.model.get_application_config("cinder-netapp")
+    family = configs['netapp-storage-family']['value']
+    protocol = configs['netapp-storage-protocol']['value']
+    backend_name = configs['volume-backend-name']['value']
+    expected_contents = {
         'cinder-netapp': {
-            'netapp_storage_family': ['ontap_cluster'],
-            'netapp_storage_protocol': ['iscsi'],
-            'volume_backend_name': ['cinder_netapp'],
+            'netapp_storage_family': [family],
+            'netapp_storage_protocol': [protocol],
+            'volume_backend_name': [backend_name],
             'volume_driver':
                 ['cinder.volume.drivers.netapp.common.NetAppDriver'],
         }}
+
+    def check_volume_host(self, volume):
+        """Validate the volume id from the expected backend.
+
+        :param volume: Volume to check
+        :type volume: cinderclient.v3.volumes.Volume
+        """
+        host = getattr(volume, 'os-vol-host-attr:host')
+        self.assertEqual(
+            host.split('#')[0].split('@')[1],
+            'cinder-netapp')
