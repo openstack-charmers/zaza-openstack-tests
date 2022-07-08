f8de01896b57fa15ba8d8d0e68efb60b9e41b2dd
Alex Kavanagh <alex.kavanagh@canonical.com>
2022-07-08 11:03:04 +0100

    Ensure unittests use zaza non-threaded
    zaza normally runs libjuju async functions in a background thread
    to enable zaza to be blocking, but keep the libjuju async functions
    'ticking over' between blocking calls.
    
    This can break unit tests as starting the zaza background thread will
    hang unless it is cleaned up.  A simple solution is just to use zaza in
    non-threaded mode in the unit tests, and zaza provides a mechanism for
    this via zaza.RUN_LIBJUJU_IN_THREAD = False.
    
    (cherry picked from commit 7c8a190e7aa6c20403654344854a5259e00fd1e6)
    (cherry picked from commit a1b6b9addcd581ff7b62a11036e45a981298bfb5)

--- unit_tests/__init__.py
+++ unit_tests/__init__.py
@@ -11,3 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# force zaza to not use threads for async
+import zaza
+
+zaza.RUN_LIBJUJU_IN_THREAD = False
