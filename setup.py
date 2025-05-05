ce301ed5676b94a2978fc81d7e6fd6a4beda6d2a
Felipe Reyes <felipe.reyes@canonical.com>
2025-05-05 17:10:59 -0400

    Pin osc-lib<4.0.0
    osc-lib-4.0.0 introduced typing hints making it incompatible with py38.
    
    Ref: https://github.com/openstack/osc-lib/commit/3d221e5992a74d6bb7d1b725e43015b8c87b9718
    
    Resolved Conflicts:
    	setup.py
    
    (cherry picked from commit dd72623e0622addc7c46b9c9ae5ae5c8254999c6)

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ install_require = [
     'tenacity',
     'oslo.config<6.12.0',
     'pyparsing<3.0.0',  # pin for aodhclient which is held for py35
+    'osc-lib<4.0.0',  # needed for py38
     'aodhclient<1.4.0',
     'gnocchiclient>=7.0.5,<8.0.0',
     'pika>=1.1.0,<2.0.0',
